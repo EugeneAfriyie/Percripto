01551ca8830f4545aa1ae91937146e0186a4f2e6
Eugene Afriyie <groupeight00@gmail.com>
2026-01-23 16:22:32 -0700

    media

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      Testing TailwindCSS
-      <div className="text-center text-blue-500 font-bold text-2xl">
-        Hello, Tailwind!
-      </div>
+    
     </div>
   )
 }
