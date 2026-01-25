0542521c458b312bae5f6555e40cc8253f5629ff
Eugene Afriyie <groupeight00@gmail.com>
2026-01-25 03:15:23 -0700

    Add routing and new pages for user navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my_profile' element={<My_Profile />} />
+        <Route path='/my_appointment' element={<My_Appointment />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     
     </div>
   )
