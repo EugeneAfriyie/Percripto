20ec0440ff122e6f8582c6409691331a11fe1487
Eugene Afriyie <groupeight00@gmail.com>
2026-01-27 15:20:36 -0700

    Speciality_Menu

---
+++ Frontend/src/components/Speciality_Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Speciality_Menu = () => {
+  return (
+    <div>Speciality_Menu</div>
+  )
+}
+
+export default Speciality_Menu
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Speciality_Menu from '../components/Speciality_Menu'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Speciality_Menu />
 
     </div>
   )
